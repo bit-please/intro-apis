aaf0b3cc4b2662852c1ff5fb84801b244a70088b
danizaghian <dani.zaghian@gmail.com>
2019-05-03 10:03:07 -0700

    adds weather program

---
+++ weather.rb
@@ -0,0 +1,15 @@
+require "http"
+
+system "clear"
+puts "Welcome to the Weather App!"
+puts "Enter a city: "
+city = gets.chomp
+
+response = HTTP.get("http://api.openweathermap.org/data/2.5/weather?q=#{city}&units=imperial&APPID=9beb296210c3171be00be7e99d0e7ba8")
+
+weather_data = response.parse
+
+temperature = weather_data["main"]["temp"]
+description = weather_data["weather"][0]["description"]
+
+puts "Today in #{city} it is #{temperature} degrees outside with #{description}."
